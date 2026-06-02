0b2f4ee5c5879d64555af7cec8be3b11931c0df4
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-06-02 14:46:51 +0100

    failing test for db connection

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI,Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
+connection = None
+
 app = FastAPI()
 templates=Jinja2Templates(directory="templates")
 
